6a5bc333285bf8e97c6e04e7fb07e00b4dcf39b7
Sim Gill <simgill89@gmail.com>
2017-03-13 10:00:04 -0700

    g

---
+++ js/actions/index.js
@@ -0,0 +1,43 @@
+// things that i need to do in this game 
+// need to able to enter an number 
+// need to render that number at the bottom
+// generate a new number
+
+
+//input data---
+//select submit---
+//start new game--
+// get description
+
+export const GUESS_NUMBER_ON_SUBMIT = 'GUESS_NUMBER_ON_SUBMIT';
+export const guessNumber = number => ({
+    type: GUESS_NUMBER_ON_SUBMIT,
+    number
+});
+
+
+
+export const ENTER_NUMBER_DATA = 'ENTER_NUMBER_DATA';
+export const Input_number = (num) => ({
+    type: ENTER_NUMBER_DATA,
+    value
+    
+});
+
+
+
+export const START_NEW_GAME_BTN = 'START_NEW_GAME_BTN';
+export const NewGame = (num) => ({
+    type: START_NEW_GAME_BTN,
+    
+    
+});
+
+
+
+export const WHAT_DESCRIPTION_BTN = 'WHAT_DESCRIPTION_BTN';
+export const decriptionBtn = (state) => ({
+    type: WHAT_DESCRIPTION_BTN,
+    
+    
+});
--- js/index.js
+++ js/index.js
@@ -1,2 +1,43 @@
 require('babel-polyfill');
 
+import * as actions from './actions/index';
+
+
+function GenerateRandomNumber() {
+
+}
+
+
+
+const initialHotColdAppState = {
+	temperture:['Make your guess'],
+	randomNumber: randomNumber,
+	GuessNumber: 0,
+	GuessHistory: [],
+	DescriptionData:
+
+
+};
+
+
+
+export const repositoryReducer = (state=initialRepositoryState, action) => {
+
+    if (action.type === guessNumber) {
+        return [...state, {
+            guessNumber: action.guessNumber
+        }];
+    }
+    
+
+
+
+
+
+
+
+
+
+
+    return state;
+};
